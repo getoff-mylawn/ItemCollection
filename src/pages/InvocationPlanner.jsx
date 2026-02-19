809d087043265fbf819372445469659cc3b66417
getoff-mylawn <trapxaxholics@gmail.com>
2026-02-19 12:08:53 -0500

    Delete src/pages/InvocationPlanner.jsx

--- src/pages/InvocationPlanner.jsx
+++
@@ -1,9 +0,0 @@
-export default function InvocationPlanner(){
-    return (
-        <>
-        <h1>InvocationPlanner</h1>
-        <span className="subtitle">Adapted from "Solo ToA: Learning 400s from scratch (post-changes)" by Parasailer — made by Ladlor</span>
-        <p>Input raid level target (150-400):</p>
-        </>
-    )
-}
